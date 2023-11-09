6ec4bedd24001eade661058ce4f406ef755dbdcd
xiaowen-chen <chenxiaowen0829@gmail.com>
2023-11-09 15:04:37 +0100

    Add files via upload

---
+++ matlab_code/misctools/model1_escp_notunnel.m
@@ -0,0 +1,28 @@
+function [edgeStruct, nodeMap, edgeMap] = model1_escp_notunnel(nNodes, nStates)
+% xiaowen chen 20200-1-13
+% escp = equal state, constant parameters
+
+
+maxState = nStates;
+maxIter = 1e4;
+
+%% construct the graphic model using UGM
+
+adj = ones(nNodes,nNodes)-diag(ones(nNodes,1));
+edgeStruct = UGM_makeEdgeStruct(adj, nStates);
+edgeStruct.maxIter=maxIter;
+nodeMap=zeros(nNodes,maxState,'int32');
+for iState=1:nStates-1
+    nodeMap(:,iState)=1+nNodes*(iState-1):nNodes*iState;
+end
+nEdges=edgeStruct.nEdges;
+edgeMap=zeros(maxState,maxState,nEdges,'int32');
+for e = 1:edgeStruct.nEdges
+    for iq = 1:nStates
+        edgeMap(iq,iq,e)=e+nNodes*(nStates-1);
+    end
+end
+
+
+
+end
