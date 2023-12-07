b41ddfb45ec1719636d9d3dcbfdc040309229ee1
xiaowen-chen <chenxiaowen0829@gmail.com>
2023-12-07 15:52:26 +0100

    Update makeUpt.m

--- matlab_code/misctools/makeUpt.m
+++ matlab_code/misctools/makeUpt.m
@@ -1,12 +1,7 @@
-% XC 20170306
+function [output] = makeUpt(input)
 % Given the upper triangular part of a matrix in the format of 1d array,
 % recover the matrix
 
-function [output] = makeUpt(input)
-%     
-%     output = triu(input,1);
-%     output = reshape(output', length(input)^2,1);
-%     output = output(output>0);
     
     l=size(input,1);
     output = zeros(l*(l-1)/2,1);
